2a0e3963964d787a376055ec9b3a55878e4a4d6c
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-16 11:00:08 +0300

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include"functions.h"
+#include "spaceship.h"
+#include "SpaceshipTypes.h"
+
+void RandomNumberGenerator(int num1, int num2, int num3, int num4) {
+    num1 = time(0);
+    srand(num1);
+    num2 = num3 + rand() % num4;
+}
+
+
+std::shared_ptr<Spaceship> selectedship() {
+    int shipSelector;
+    static std::shared_ptr<Spaceship> selectedShip;
+std:cout << "Please select your ship type. \n";
+std:cout << "1-Balanced ship\n";
+std:cout << "2-Fast ship\n";
+std:cout << "3-Powerful ship\n";
+std:cin >> shipSelector;
+
+
+
+    if (shipSelector == 1) {
+        selectedShip = make_shared<BalancedShip>(50, 1);
+    }
+    if (shipSelector == 2) {
+        selectedShip = make_shared<FastShip>(75, 1.5);
+    }
+    if (shipSelector == 3) {
+        selectedShip = make_shared<PowerfulShip>(25, 0.5);
+    }
+    else {
+    std:cout << "Please select a valid number.\n";
+    }
+
+
+}
