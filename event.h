1886360a5a3fedc5ac13b29252bdb22605a9f8d3
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-16 10:58:24 +0300

    Create event.h

---
+++ event.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "spaceship.h"
+#include "functions.h"
+using namespace std;
+
+class events {
+public:
+	void AstreoidBelt();
+	void AbondenedPlanet();
+	void SpacePirates();
+};
