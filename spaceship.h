d5b262c735f5471cb0690c33d6d2723b236c7338
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-16 10:58:59 +0300

    Create spaceship.h

---
+++ spaceship.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+
+class Spaceship {
+public:
+    Spaceship() = default;
+    Spaceship(int _flee, int _damageMultiplier) : flee_posibility{ _flee }, damageMultiplier{ _damageMultiplier } {};
+
+    inline float getHp() const {
+        return Hp;
+    }
+    inline int getFuel() const {
+        return fuel;
+    }
+    inline int getCash() const {
+        return cash;
+    }
+    inline void setHp(float _newHp) {
+        Hp = _newHp;
+    }
+    inline void setFuel(int _newFuel) {
+        fuel = _newFuel;
+    }
+    inline void setCash(int _newCash) {
+        cash = _newCash;
+    }
+    inline float getDamageMultiplier() const {
+        return damageMultiplier;
+    }
+    inline int getEscapePossibility() const {
+        return flee_posibility;
+    }
+
+   virtual void status();
+
+
+protected:
+    float Hp{ 100 };
+    int fuel{ 100 };
+    int cash{ 0 };
+    float damageMultiplier{ 0 };
+    int flee_posibility{ 0 };
+};
