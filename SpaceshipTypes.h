51ddbc846404d96762328ce757ded83d9429c730
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-17 01:53:06 +0300

    Update SpaceshipTypes.h

--- SpaceshipTypes.h
+++ SpaceshipTypes.h
@@ -1,33 +1,29 @@
 #pragma once
-#include <iostream>
 #include "Spaceship.h"
 
-
-class FastShip :public Spaceship {
+class FastShip : public Spaceship {
 public:
     FastShip(int _flee, float _damageMultiplier) : Spaceship(_flee, _damageMultiplier) {};
-   
-    void status() {
-        std::cout << "Hp: " << getHp << "\nFuel: " << getFuel << "\nCash: " << getCash << endl;
+
+    void status() override {
+        std::cout << "Hp: " << getHp() << "\nFuel: " << getFuel() << "\nCash: " << getCash() << std::endl;
     }
 };
 
-
-class PowerfulShip :public Spaceship {
+class PowerfulShip : public Spaceship {
 public:
     PowerfulShip(int _flee, float _damageMultiplier) : Spaceship(_flee, _damageMultiplier) {};
 
-    void status() {
-        std::cout << "Hp: " << getHp << "\nFuel: " << getFuel << "\nCash: " << getCash << endl;
+    void status() override {
+        std::cout << "Hp: " << getHp() << "\nFuel: " << getFuel() << "\nCash: " << getCash() << std::endl;
     }
 };
 
-
-class BalancedShip :public Spaceship {
+class BalancedShip : public Spaceship {
 public:
     BalancedShip(int _flee, float _damageMultiplier) : Spaceship(_flee, _damageMultiplier) {};
 
-    void status() {
-        std::cout << "Hp: " << getHp << "\nFuel: " << getFuel << "\nCash: " << getCash << endl;
+    void status() override {
+        std::cout << "Hp: " << getHp() << "\nFuel: " << getFuel() << "\nCash: " << getCash() << std::endl;
     }
 };
