96efddd2bcb2f54d421cccd17d7c0bf8550e8fa0
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-17 01:52:11 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
-#include"functions.h"
-#include "spaceship.h"
+#include "functions.h"
 #include "SpaceshipTypes.h"
+#include <ctime> 
+#include <cstdlib> 
 
-void RandomNumberGenerator(int num1, int num2, int num3, int num4) {
-    num1 = time(0);
-    srand(num1);
-    num2 = num3 + rand() % num4;
+
+int RandomNumberGenerator(int min, int max) {
+    srand(static_cast<unsigned int>(time(0))); 
+    return min + rand() % (max - min + 1);
 }
 
-
-std::shared_ptr<Spaceship> selectedship() {
+std::shared_ptr<Spaceship> selectedShip() {
     int shipSelector;
-    static std::shared_ptr<Spaceship> selectedShip;
-std::cout << "Please select your ship type. \n";
-std::cout << "1-Balanced ship\n";
-std::cout << "2-Fast ship\n";
-std::cout << "3-Powerful ship\n";
-std::cin >> shipSelector;
-
-
+    std::cout << "Please select your ship type. \n";
+    std::cout << "1-Balanced ship\n";
+    std::cout << "2-Fast ship\n";
+    std::cout << "3-Powerful ship\n";
+    std::cin >> shipSelector;
 
     if (shipSelector == 1) {
-        selectedShip = std::make_shared<BalancedShip>(50, 1);
+        return std::make_shared<BalancedShip>(50, 1);
     }
     else if (shipSelector == 2) {
-        selectedShip = std::make_shared<FastShip>(75, 1.5);
+        return std::make_shared<FastShip>(75, 1.5);
     }
     else if (shipSelector == 3) {
-        selectedShip = std::make_shared<PowerfulShip>(25, 0.5);
+        return std::make_shared<PowerfulShip>(25, 0.5);
     }
     else {
-    std::cout << "Please select a valid number.\n";
+        std::cout << "Please select a valid number.\n";
+        return nullptr; 
     }
-    return selectedShip;
 }
