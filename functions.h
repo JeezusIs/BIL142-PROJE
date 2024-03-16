73051ac4269185c4dfc15e2ce54b13e2852120e8
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-16 23:39:17 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <iostream>
 #include "Spaceship.h"
-using namespace std;
 
 void RandomNumberGenerator();
 std::shared_ptr<Spaceship> selectedShip;
 void RandomEventGenerator();
 void ScoreCalculator();
+
 
