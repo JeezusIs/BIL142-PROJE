6f09236e50f7246aeba7144d1ea9a17765414d74
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-17 03:58:57 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,3 +1,6 @@
+#ifndef BIL142_GENERALFUNCS_H
+#define BIL142_GENERALFUNCS_H
+
 #pragma once
 #include <iostream>
 #include "spaceship.h"
@@ -5,6 +8,8 @@
 #include <memory> 
  
 int RandomNumberGenerator(int num1, int num2);
-std::shared_ptr<Spaceship> selectedShip(); 
+std::shared_ptr<Spaceship> getSelectedShip(); 
 void RandomEventGenerator();
 void ScoreCalculator();
+
+#endif //BIL142_GENERALFUNCS_H
